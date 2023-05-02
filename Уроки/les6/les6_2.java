cb67a6124309d16e041f7b7f4f7115da790b25a2
Paintin1 <110852853+Paintin1@users.noreply.github.com>
2023-05-02 22:09:51 +0300

    Условные операторы

---
+++ Уроки/les6/les6_2.java
@@ -0,0 +1,14 @@
+package Уроки.les6;
+//Условные конструкции
+//Несколько условий
+public class les6_2 {
+    public static void main(String[] args){
+        int a = 15, b = 15;
+        char sym1 = 'A', sym2 = 'A';
+        boolean isHasCar = true;
+        if (isHasCar && a == b && sym1 != sym2){ // ||-используются для создания нескольких условий несёт значение ИЛИ. &&-используется анологично, но несёт в себе значение И
+            //условие выполнется если переменная isHasCar = истине и a равно b и sym1 не равно sym2
+
+        }
+    }
+}
