2f64d5e258ef23d54115e93d1adb61f8e23bdfef
Paintin1 <110852853+Paintin1@users.noreply.github.com>
2023-05-02 18:26:08 +0300

    Организация

---
+++ Уроки/les3.java
@@ -0,0 +1,9 @@
+package Уроки;
+//Вывод значений
+
+public class les3
+{
+    public static void main(String[] args){
+        System.out.println("Hello\" \\ \n \tWorld!"); //sout + enter = System.out.println();
+    }
+}
