58560e79b191f5c786b64dcde62ae13328f6acd8
Paintin1 <110852853+Paintin1@users.noreply.github.com>
2023-05-04 22:50:41 +0300

    Массивы

---
+++ Уроки/les8/les8_2.java
@@ -0,0 +1,15 @@
+package Уроки.les8;
+//Массивы данных
+public class les8_2 {
+    public static void main(String[] args) {
+        float[] nums2 = new float[] {5.0f, 6.45f, 89.984f}; //Сразу объявляем размер масива и его переменные
+        System.out.println(nums2[1]);
+
+        for (int i =0; i < nums2.length; i++){ //Для вывода всех значений массива используют циклы. Чтобы в ручную не считать количество элементов, обращаемся к массиву и используем метод length, который показывает количество элементов в массиве
+            System.out.println("Element: " + nums2[i]);
+
+        }
+
+
+    }
+}
