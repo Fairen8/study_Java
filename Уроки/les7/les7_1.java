454dafc47424596c573337b8d1d6f9cc15b18a50
Paintin1 <110852853+Paintin1@users.noreply.github.com>
2023-05-04 21:46:18 +0300

    Циклы

---
+++ Уроки/les7/les7_1.java
@@ -0,0 +1,10 @@
+package Уроки.les7;
+//Циклы
+//for()
+public class les7_1 {
+    public static void main(String[] args){
+        for (int i = 0; i <= 10; i++){ //В объявлении цикла записывается следующее: переменная цикла, её начальное значение и диапазон. В примере выше будут выведены числа от 0 до 10.
+            System.out.println("Element: " + i);
+        }
+    }
+}
