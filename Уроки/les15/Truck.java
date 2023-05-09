0ce0647f1543fc159179c7b7fc427ab7caad5030
Paintin1 <110852853+Paintin1@users.noreply.github.com>
2023-05-09 22:30:02 +0300

    Полиморфизм

---
+++ Уроки/les15/Truck.java
@@ -0,0 +1,39 @@
+package Уроки.les15;
+
+public class Truck extends Transport {
+
+    private boolean isLoaded;
+
+    public void setValues(float speed, int weight,String color, byte[] coordinate,boolean isLoaded) {
+        super.setValues(speed, weight, color, coordinate);
+        this.isLoaded = isLoaded;
+
+    }
+
+    @Override
+    protected String getValues() {
+        System.out.println(super.getValues());
+        return getLoaded();
+    }
+
+    public Truck(int weight , byte[] coordinate) {
+        super(weight, coordinate);
+    }
+
+    public Truck(int weight , byte[] coordinate, boolean isLoaded) {
+        super(weight, coordinate);
+        this.isLoaded = isLoaded;
+    }
+
+    public void setLoaded(boolean loaded) {
+        isLoaded = loaded;
+    }
+
+    public String getLoaded() {
+        if (isLoaded)
+            return "Грузовик загружен";
+        else
+            return "Грузовик не загружен";
+    }
+
+}
