1458d16279cec3296bf1fec42f9eaa8e54b7bb00
Paintin1 <110852853+Fairen8@users.noreply.github.com>
2023-05-13 22:06:35 +0300

    Вложенные и анонимные классы

---
+++ Уроки/les17/Car.java
@@ -0,0 +1,21 @@
+package Уроки.les17;
+
+public class Car extends Transport {
+
+    public Engine engine = new Engine();
+    public Car(float _speed, int _weight, String _color, byte[] _coordinate) {
+        super(_speed, _weight, _color, _coordinate);
+    }
+
+    @Override
+    public void moveObject(float speed) {
+        System.out.println("Наш объект двигается со скоростью: " + speed);
+    }
+    @Override
+    public boolean stopObject() {
+        this.speed = 0;
+        return true;
+    }
+
+
+}
