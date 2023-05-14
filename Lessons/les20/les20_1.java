96abeb7c9ef24f4d6b01aab497d56f0025f0e68f
Paintin1 <110852853+Fairen8@users.noreply.github.com>
2023-05-14 22:06:14 +0300

    Построение ПК приложений

---
+++ Lessons/les20/les20_1.java
@@ -0,0 +1,9 @@
+package Lessons.les20;
+//Построение ПК приложений
+public class les20_1 {
+
+    public static void main(String[] args) {
+        ContactForm form = new ContactForm();
+        form.setVisible(true);
+    }
+}
