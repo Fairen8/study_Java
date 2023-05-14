ac383399bb764c1ceed6b49589527b00f258756f
Paintin1 <110852853+Fairen8@users.noreply.github.com>
2023-05-14 12:43:00 +0300

    Интерфейсы в Джава

---
+++ Lessons/les19/ILights.java
@@ -0,0 +1,8 @@
+package Lessons.les19;
+
+public interface ILights {
+
+    void setLite(boolean set);
+    void blinkLight();
+
+}
---
+++ Lessons/les19/les19_1.java
@@ -0,0 +1,10 @@
+package Lessons.les19;
+//Интерфейсы в Джава
+public class les19_1 {
+    public static void main(String[] args) {
+        Car bmv = new Car();
+
+        bmv.setLite(true);
+        bmv.blinkLight();
+    }
+}
