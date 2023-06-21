1914364d667221285c02be2c899b5b58833de83c
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-06-21 16:43:59 +0300

    Массивы

---
+++ Paid_Lessons/Lessons/les4/les4_1.java
@@ -0,0 +1,13 @@
+package Paid_Lessons.Lessons.les4;
+//Массивы
+public class les4_1 {
+    public static void main(String[] args) {
+        byte[] nums_array = new byte[5];
+        System.out.println(nums_array.length);
+
+
+        String[] names = new String[] {"Пётр", "Андрей", "Гоша" };
+        names[0] = "Вова";
+        System.out.println(names[0]);
+    }
+}
