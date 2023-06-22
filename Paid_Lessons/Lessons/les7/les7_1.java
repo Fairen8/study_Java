b6b2ffde2491addafecca8fbe15e251e4070320b
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-06-22 16:44:23 +0300

    Исключения

--- Paid_Lessons/Lessons/les7/les7_1.java
+++ Paid_Lessons/Lessons/les7/les7_1.java
@@ -1,5 +1,25 @@
 package Paid_Lessons.Lessons.les7;
 
 public class les7_1 {
-    public sta
+    public static void main(String[] args) {
+        float[] arr = new float[] {5.3f, 66.46f, 264.46f,};
+
+        try { //Блок в котором отслеживаются ошибки
+            System.out.println(arr[3]);
+            int res = 100 / 0;
+        } catch (ArrayIndexOutOfBoundsException e) { //Блок действий в случае ошибки
+            System.out.print("Ошибка: " + e); //е объект в который схраняется ошибка
+        } catch (ArithmeticException e) { //Проверка на несколько ошибок
+            System.out.print("Ошибка: " + e); //Exception - все исключения
+        }finally {
+            /*
+            Блок который срабатывает в любом случае
+            после завершения блока try
+            или
+            после завершения блока catch
+             */
+            System.out.print("сработал блок finally");
+        }
+
+    }
 }
