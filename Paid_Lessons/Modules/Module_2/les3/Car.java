59e6bb87fcca0f351b6d263d650521f69c4c460f
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-06-24 00:15:11 +0300

    Абстрактные классы и методы

---
+++ Paid_Lessons/Modules/Module_2/les3/Car.java
@@ -0,0 +1,28 @@
+package Paid_Lessons.Modules.Module_2.les3;
+
+abstract class Car { //С помощью абстрактных классов нельзя создавать объекты
+    protected short speed;
+    private String model;
+    private float weight;
+    private String color;
+    private boolean isWork;
+
+    Car(String model, float weight, String color, boolean isWork) {
+        this.model = model;
+        this.weight = weight;
+        this.color = color;
+        this.isWork = isWork;
+    }
+
+    void printAll() {
+        String isWork = this.isWork ? "работает" : "не работает";
+        System.out.println("Автомобиль марки: " + this.model +
+                "\nЦвет: " + this.color +
+                "\nВес: " + this.weight +
+                "\nСкорость: " + this.speed +
+                "\nИ он " + isWork);
+    }
+
+    abstract void startEngine(); //Абстрактные обязательно должны быть реализованы в классах наследниках
+                                 //Но не мгут быть реализованы в супер классе
+}
---
+++ Paid_Lessons/Modules/Module_2/les3/les3_1.java
@@ -0,0 +1,10 @@
+package Paid_Lessons.Modules.Module_2.les3;
+
+public class les3_1 {
+    public static void main(String[] args) {
+        BMW m5 = new BMW(true, "m5", 2500.5f, "Синий", true, (short) 200);
+
+        m5.printAll();
+    }
+
+}
