823e95cec51e09cfffeed54ef8df8802d19d12b6
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-06-27 04:22:24 +0300

    Сериализация

---
+++ Paid_Lessons/Modules/Module_3/les4/les4_1/Main.java
@@ -0,0 +1,26 @@
+package Paid_Lessons.Modules.Module_3.les4.les4_1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Main {
+    //В этом классе мы записываем данные в файл
+    public static void main(String[] args) {
+        Course ob = new Course("Kotlin", 1);
+        Course android = new Course("Android", 5);
+
+        try {
+            FileOutputStream fos = new FileOutputStream("Paid_Lessons/Modules/Module_3/les4/les4_1/main.i"); //Файл может иметь любое расширение
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+
+            oos.writeObject(android);
+            oos.writeObject(ob);
+
+            oos.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
