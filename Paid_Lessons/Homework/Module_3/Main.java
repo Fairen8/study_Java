136ae1d188feca6cf2ac0026ad234a4351130a0a
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-06-28 00:19:48 +0300

    Домашка

---
+++ Paid_Lessons/Homework/Module_3/Main.java
@@ -0,0 +1,52 @@
+package Paid_Lessons.Homework.Module_3;
+
+import java.io.*;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите имя: ");
+        String name = in.nextLine();
+
+        System.out.print("Введите логин: ");
+        String login = in.nextLine();
+
+        System.out.print("Введите возраст: ");
+        byte age = in.nextByte();
+
+        System.out.print("Введите хобби через запятую: ");
+        String hobby_1 = in.nextLine();
+        String hobby = in.nextLine();
+
+        User user = new User(name, login, age, List.of(hobby.split(",")));
+
+        try {
+            FileOutputStream fos = new FileOutputStream("Paid_Lessons/Homework/Module_3/some.data");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(user);
+
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileInputStream fis = new FileInputStream("Paid_Lessons/Homework/Module_3/some.data");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            User obj = (User) ois.readObject();
+
+            System.out.println(obj);
+
+            ois.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
