a1667414bb99a5be5c541963486c55c77514112f
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-06-27 04:57:54 +0300

    Лямбда выражения

---
+++ Paid_Lessons/Modules/Module_3/les5/les5_2/IMath.java
@@ -0,0 +1,6 @@
+package Paid_Lessons.Modules.Module_3.les5.les5_2;
+
+@FunctionalInterface //Мы говорим что в этом интерфейсе будет только одна функция
+public interface IMath {
+    int summ (int a, int b);
+}
---
+++ Paid_Lessons/Modules/Module_3/les5/les5_2/les5_2.java
@@ -0,0 +1,19 @@
+package Paid_Lessons.Modules.Module_3.les5.les5_2;
+
+
+public class les5_2 {
+    public static void main(String[] args) {
+        IMath iMath = new IMath() {
+            @Override
+            public int summ(int a, int b) {
+                int sum = a + b;
+                return sum;
+            }
+        };
+
+        System.out.println(iMath.summ(5, 7));
+
+        IMath iMath1 = (a, b) -> a + b;
+        System.out.println(iMath1.summ(6, 7));
+    }
+}
