7f073164667641dcced2fbf4ba3220d9e3eddf00
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-07-21 16:00:28 +0300

    Spring проэкт
    Подключил БД и описал одну таблицу items

---
+++ Paid_Lessons/Modules/Module_11/src/main/java/com/example/Module_11/models/item.java
@@ -0,0 +1,58 @@
+package com.example.Module_11.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title, info, image;
+
+    private short price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public short getPrice() {
+        return price;
+    }
+
+    public void setPrice(short price) {
+        this.price = price;
+    }
+}
