462b6081d696d9f13cb1a99ddced53d763a9e36c
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-07-25 22:37:48 +0300

    Spring проэкт-домашка
    Добавил систему изменений данных пользователя

---
+++ Lessons_2/Homework/Module_12/src/main/java/com/example/Module_12/controllers/UserController.java
@@ -0,0 +1,70 @@
+package com.example.Module_12.controllers;
+
+import com.example.Module_12.models.Role;
+import com.example.Module_12.models.User;
+import com.example.Module_12.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.util.Collections;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/user")
+    public String user(Principal principal, Model model) {
+        User user = userRepository.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+        return "user";
+    }
+
+    @PostMapping("/user/update")
+    public String updateUser(Principal principal, User userForm) {
+        User user = userRepository.findByUsername(principal.getName());
+        user.setEmail(userForm.getEmail());
+        user.setRoles(userForm.getRoles());
+        if (userForm.getPassword().length() != 0)
+            user.setPassword(passwordEncoder.encode(userForm.getPassword()));
+        userRepository.save(user);
+        return "redirect:/user";
+    }
+
+
+    @GetMapping("/reg")
+    public String reg(@RequestParam(name = "error", defaultValue = "", required = false) String error, Model model) {
+        if (error.equals("username"))
+            model.addAttribute("error", "Такой логин пользователя уже занят");
+        return "reg";
+    }
+
+    @PostMapping("/reg")
+    public String addUser(@RequestParam String username,
+                          @RequestParam String email,
+                          @RequestParam String password) {
+
+        if (userRepository.findByUsername(username) != null)
+            return "redirect:/reg?error=username";
+
+        User user = new User(username, passwordEncoder.encode(password), email, true, Collections.singleton(Role.USER));
+
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
---
+++ Lessons_2/Homework/Module_12/src/main/java/com/example/Module_12/models/Role.java
@@ -0,0 +1,5 @@
+package com.example.Module_12.models;
+
+public enum Role {
+    USER, ADMIN, REDACTOR
+}
