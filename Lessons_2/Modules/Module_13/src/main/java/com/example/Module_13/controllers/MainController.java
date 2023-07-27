9c1503c71b89444c9931c73b2c60161614f9e781
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-07-27 13:12:58 +0300

    Spring проэкт
    Добавил обработку ошибки 404. Включил scfr токен.

---
+++ Lessons_2/Modules/Module_13/src/main/java/com/example/Module_13/controllers/MainController.java
@@ -0,0 +1,28 @@
+package com.example.Module_13.controllers;
+
+
+import com.example.Module_13.repo.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("items", itemRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/about-us")
+    public String about(@RequestParam(name = "userName", required = false, defaultValue = "World") String userName, Model model) {
+        model.addAttribute("name", userName);
+        return "about";
+    }
+}
---
+++ Lessons_2/Modules/Module_13/src/main/java/com/example/Module_13/models/User.java
@@ -0,0 +1,105 @@
+package com.example.Module_13.models;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String username, password, email;
+
+    private boolean enable;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    public User() {}
+
+    public User(String username, String password, String email, boolean enable, Set<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.enable = enable;
+        this.roles = roles;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
