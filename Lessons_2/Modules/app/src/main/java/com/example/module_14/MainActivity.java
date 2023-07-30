4a09ce7e9213c54b704b012350c4513622276f39
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-07-30 23:26:01 +0300

    Android приложение
    Создал небольшое приложение-конвертер

--- Lessons_2/Modules/app/src/main/java/com/example/module_14/MainActivity.java
+++ Lessons_2/Modules/app/src/main/java/com/example/module_14/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.module_14;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void ConvertSpeed(View v) {
+        EditText user_textfielt = findViewById(R.id.user_textfielt);
+        TextView result_textview = findViewById(R.id.result_textview);
+
+        String text = user_textfielt.getText().toString();
+        if (!text.equals("")) {
+            float numer = Float.parseFloat(text);
+            numer *= 1.6;
+            result_textview.setText(Float.toString(numer));
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
