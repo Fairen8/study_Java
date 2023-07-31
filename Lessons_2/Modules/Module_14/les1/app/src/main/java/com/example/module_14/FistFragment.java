3478a4ca48eb2c1dfd833d0fab4afc5305b5f620
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-07-31 17:51:43 +0300

    Android проэкт
    Создал динамическое содержимое на вторую страницу приложения

---
+++ Lessons_2/Modules/Module_14/les1/app/src/main/java/com/example/module_14/FistFragment.java
@@ -0,0 +1,18 @@
+package com.example.module_14;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FistFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fist, container, false);
+    }
+}
--- Lessons_2/Modules/Module_14/les1/app/src/main/java/com/example/module_14/SecondActivity.java
+++ Lessons_2/Modules/Module_14/les1/app/src/main/java/com/example/module_14/SecondActivity.java
@@ -1,8 +1,12 @@
 package com.example.module_14;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    public void FragmentChange(View v) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (v.getId() == R.id.fragment_btn_1) {
+            FistFragment fistFragment = new FistFragment();
+            fragmentTransaction.replace(R.id.fragmentContainerView, fistFragment);
+        } else if (v.getId() == R.id.fragment_btn_2) {
+            SecondFragment secondFragment = new SecondFragment();
+            fragmentTransaction.replace(R.id.fragmentContainerView, secondFragment);
+        }
+        fragmentTransaction.commit();
     }
 }
