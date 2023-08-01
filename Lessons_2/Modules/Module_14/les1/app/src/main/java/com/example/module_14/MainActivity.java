5eb1d764e0ed6ed0b88a2e33602fb6833a6192eb
Fairen8 <110852853+Fairen8@users.noreply.github.com>
2023-08-01 17:30:17 +0300

    Android проэкт
    Добавил отображение для альбомного отображения + работа с картинками

--- Lessons_2/Modules/Module_14/les1/app/src/main/java/com/example/module_14/MainActivity.java
+++ Lessons_2/Modules/Module_14/les1/app/src/main/java/com/example/module_14/MainActivity.java
@@ -6,12 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText user_textfielt;
     private TextView result_textview;
     private ImageButton btn_next_activity;
+    private ImageView imageViewSimple,imageViewVector;
+
+    private int count = 0;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -32,30 +37,57 @@ public class MainActivity extends AppCompatActivity {
         user_textfielt = findViewById(R.id.user_textfielt);
         result_textview = findViewById(R.id.result_textview);
         btn_next_activity = findViewById(R.id.btn_next_activity);
+        imageViewVector = findViewById(R.id.imageViewVector);
+        imageViewSimple = findViewById(R.id.imageViewSimple);
 
-        conver_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage("Вы хотите произвести конвертацию");
-                builder.setCancelable(false);
-                builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        ConvertSpeed();
+        int orientation = this.getResources().getConfiguration().orientation;
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            imageViewVector.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    count++;
+                    count = count > 3 ? 1 : count;
+                    switch (count) {
+                        case 1:
+                            imageViewSimple.setImageResource(R.drawable.boykisser1);
+                            break;
+                        case 2:
+                            imageViewSimple.setImageResource(R.drawable.boykisser3);
+                            break;
+                        case 3:
+                            imageViewSimple.setImageResource(R.drawable.boykisser2);
+                        default:
+                            imageViewSimple.setImageResource(R.drawable.boykisser2);
                     }
-                });
-                builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), "Вы отменили действие", Toast.LENGTH_LONG).show();
-                    }
-                });
-                AlertDialog alertDialog = builder.create();
-                alertDialog.setTitle("Конвертация данных");
-                alertDialog.show();
-            }
-        });
+                }
+            });
+        }
+
+        if(orientation == Configuration.ORIENTATION_PORTRAIT) {
+            conver_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Вы хотите произвести конвертацию");
+                    builder.setCancelable(false);
+                    builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ConvertSpeed();
+                        }
+                    });
+                    builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(getApplicationContext(), "Вы отменили действие", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.setTitle("Конвертация данных");
+                    alertDialog.show();
+                }
+            });
+        }
 
         btn_next_activity.setOnClickListener(new View.OnClickListener() {
             @Override
